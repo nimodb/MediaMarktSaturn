f19f045452c91df3e7be93612d193191e91888ce
Nima Saadati <nimodbdev@gmail.com>
2024-06-07 12:55:19 +0400

    Proper validation and error handling for all endpoints

--- security/serializers.py
+++ security/serializers.py
@@ -6,3 +6,19 @@ class SecurityRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = SecurityRecord
         fields = ["id", "name", "description"]
+
+    def validate(self, attrs):
+        # validate name
+        name = attrs.get("name", "")
+        if len(name) < 5:
+            raise serializers.ValidationError(
+                "Name must be at least 5 characters long."
+            )
+
+        # validate description
+        description = attrs.get("description", "")
+        if len(description) < 10:
+            raise serializers.ValidationError(
+                "Description must be at least 10 characters long."
+            )
+        return super().validate(attrs)
--- security/views.py
+++ security/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
 from .models import SecurityRecord
 from .serializers import SecurityRecordSerializer
 
@@ -7,3 +9,21 @@ from .serializers import SecurityRecordSerializer
 class SecurityRecordViewSet(viewsets.ModelViewSet):
     queryset = SecurityRecord.objects.all()
     serializer_class = SecurityRecordSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

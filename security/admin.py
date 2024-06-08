7d90097dabb80ae85b773585bd82cabba60ac5e1
Nima Saadati <nimodbdev@gmail.com>
2024-06-08 18:03:22 +0400

    Implement SecurityRecordAdmin in admin.py

--- security/admin.py
+++ security/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import SecurityRecord
+
 
 # Register your models here.
+@admin.register(SecurityRecord)
+class SecurityRecordAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name", "description"]
+    list_per_page = 10
+    ordering = ["-id"]
